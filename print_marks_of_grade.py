6b130142a94bafd11112fe7ac46931502bab9402
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-06 21:50:47 +0530

    Add grade classification based on marks input

---
+++ print_marks_of_grade.py
@@ -0,0 +1,11 @@
+marks=int(input())
+if marks >= 90 and marks <=100:
+    print("Grade A")
+elif marks >= 80 and marks < 90:
+    print("Grade B")
+elif marks >=70 and marks < 80:
+    print("Grade C")
+elif marks >=60 and marks < 50:
+    print("Grade D")
+else:
+    print("Fail")
