1b8b28ebed4eb903985c620edcea0179a86deaf9
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-11 14:16:45 +0530

    Add files via upload

---
+++ number_is_palindrome_or_not.py
@@ -0,0 +1,11 @@
+n=int(input())
+temp=n
+rev=0
+while temp>0:
+    digit=temp%10
+    rev=rev*10+digit
+    temp=temp//10
+if n==rev:
+    print(n, "is a palindrome")
+else:
+    print(n,"is not a palindrome")
