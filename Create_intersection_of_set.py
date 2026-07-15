65b80a89b1cb45216efb9802501856abc302a4cd
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-15 22:41:02 +0530

    Add files via upload

---
+++ Create_intersection_of_set.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter number of elements in set 1: "))
+s1=set()
+for i in range(n1):
+    x=int(input("Enter element: "))
+    s1.add(x)
+n2=int(input("Enter number of elements in set 2: "))
+s2=set()
+for i in range(n2):
+    x=int(input("Enter element: "))
+    s2.add(x)
+print("Intersection of sets:", s1.intersection(s2))
