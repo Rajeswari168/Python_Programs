25a18cddd9ffb29adfd854bd66f0e98553bd0e57
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-20 09:51:52 +0530

    Add files via upload

---
+++ Open_file_and_print_content.py
@@ -0,0 +1,12 @@
+try:
+    filename = input("Enter file name: ")
+
+    file = open(filename, "r")
+    content = file.read()
+
+    print(content.title())
+
+    file.close()
+
+except FileNotFoundError:
+    print("Error: File not found.")
