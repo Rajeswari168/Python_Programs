6ac7b5b40965d434093666342192261447320292
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-20 09:50:51 +0530

    Add files via upload

---
+++ Check_prime_with_exception_handling.py
@@ -0,0 +1,19 @@
+try:
+    n = int(input("Enter a number: "))
+
+    if n < 2:
+        print("Not Prime")
+    else:
+        prime = True
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                prime = False
+                break
+
+        if prime:
+            print("Prime Number")
+        else:
+            print("Not Prime")
+
+except ValueError:
+    print("Error: Please enter a valid number.")
